c83f0dd4259768beef2ad7892e193d929ecd2644
nicolatl <lawford.nicola@gmail.com>
2022-11-23 18:42:18 -0500

    update code formatting using flake8 and black

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,5 +1,4 @@
 import pytest
-import os
 import json
 from pathlib import Path
 
@@ -74,6 +73,7 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
