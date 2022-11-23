5fd41f95fca7811ae9f726ca4dabafeece7f1e9b
nicolatl <lawford.nicola@gmail.com>
2022-11-23 18:29:37 -0500

    require login to delete messages

--- project/app.py
+++ project/app.py
@@ -25,7 +25,16 @@ app.config.from_object(__name__)
 db = SQLAlchemy(app)
 
 from project import models
+from functools import wraps
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/')
 def index():
@@ -71,11 +80,13 @@ def logout():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db.session.query(models.Post).filter_by(id=post_id).delete()
+        new_id = post_id
+        db.session.query(models.Post).filter_by(id=new_id).delete()
         db.session.commit()
         result = {'status': 1, 'message': "Post Deleted"}
         flash('The entry was deleted.')
